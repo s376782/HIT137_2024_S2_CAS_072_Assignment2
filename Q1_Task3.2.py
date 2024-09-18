fe8cd9eb717fffa3aa807f99a2a54565a1d9ccaa
Tran Bao Minh Vu <tran.vu@students.cdu.edu.au>
2024-09-18 19:04:51 +0930

    update code

--- Q1_Task3.2.py
+++ Q1_Task3.2.py
@@ -10,58 +10,53 @@ import csv
 # Here we use the Python version to ver 3.10 for this assignment in order to install spispaCy library 
 warnings.filterwarnings("ignore", category=FutureWarning)   
 
-class CTokenizer:
+class TokenizerProcessor:
     def __init__(self, model_name='bert-base-uncased'):    # Split text into tokens following BERT's tokenization rules.
         self.__tokenizer = AutoTokenizer.from_pretrained(model_name)    
 
-    def __process_chunk(self, chunk):
+    def __process_chunk(self, chunk, chunk_counter, token_counts):
         # Process the chunk
-        self.__chunk_counter += 1
-        print(f"Processing chunk {self.__chunk_counter}...")
+        print(f"Processing chunk {chunk_counter}...")
 
         tokens = self.__tokenizer.tokenize(chunk)
             
         # Update the token counter
-        self.__token_counts.update(tokens)
-
-        # Update the total tokens processed
-        self.__total_tokens_processed += len(tokens)
-        print(f"Chunk {self.__chunk_counter} processed. Total tokens processed: {self.__total_tokens_processed}")
-
-        return True
+        token_counts.update(tokens)
 
     def process(self, file_path):
         # Initialize a counter for token frequencies
-        self.__token_counts = Counter()
+        token_counts = Counter()
 
         # Initialize variables to track progress
-        self.__chunk_counter = 0
-        self.__total_tokens_processed = 0
+        chunk_counter = 0
 
         with p.ThreadPool(mp.cpu_count()) as pool:    # process text chunks concurrently to speed up the processing.
-            with open(file_path, 'r') as file:
+            with open(file_path, 'r', encoding='utf-8') as f:
                 while True:
-                    chunk = file.read(1024*1024)    # Read chunk with size of 1024*1024 (1MB)
+                    # Read chunk with size of 1024*1024 (1MB)
+                    chunk = f.read(1024*1024)
                     if not chunk:
                         break
-                    pool.apply_async(self.__process_chunk, args=(chunk,))
+
+                    chunk_counter += 1
+
+                    pool.apply_async(self.__process_chunk, args=(chunk, chunk_counter, token_counts))
 
             pool.close()
             pool.join()
 
-        # Get the top 30 most common tokens
-        top_30 = self.__token_counts.most_common(30)
-        return top_30
+        return token_counts;
 
 # File path for the large .txt file
 file_path = "Q1_Task1_all_text_output.txt"
 
 # Process the file and extract diseases and drugs
-x = CTokenizer()
-top_30 = x.process(file_path)
+processor = TokenizerProcessor()
+# Get the top 30 most common tokens
+top_30 = processor.process(file_path).most_common(30)
 
 #Store result of top 30 common words in csv file: Q1_Task3.2_Top_30_common_words.csv
-with open('Q1_Task3.2_Top_30_common_words_AutoTokenizer.csv','w',encoding='utf-8') as result:
+with open('Q1_Task3.2_Top_30_common_words_AutoTokenizer.csv', 'w', encoding='utf-8') as result:
     top30_file = csv.writer(result) #Write data into CSV file
     top30_file.writerow(['Common word', 'Count frequency']) #Write name of the header
     top30_file.writerows(top_30) #Add value of top 30 common words in rows of csv file
