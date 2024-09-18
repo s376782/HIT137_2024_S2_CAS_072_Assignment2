c612c505f2236a82f2ce3a2ba48948e49f0b8e80
Chitt <tranthuychi3597@gmail.com>
2024-09-18 12:01:03 +0930

    update to create function

--- Q1_Task1.py
+++ Q1_Task1.py
@@ -2,30 +2,30 @@
 import pandas as pd
 
 #Initiate empty list to store text data
-alltext = []
+def extract_text_from_csv(dataset, output_file):
+    alltext = []
 
-#Create list to store all dataset
-dataset = ['data/CSV1.csv', 'data/CSV2.csv', 'data/CSV3.csv','data/CSV4.csv']
+    #Collect all text by a loop through each dataset
+    for file in dataset:
+        #Read each CSV file
+        df = pd.read_csv(file)
+    
+        #Extract the columns contain name "TEXT" in the column name
+        text_columns = df.filter(like = 'TEXT', axis = 1)
+    
+        #Convert the extracted columns from series to strings, then to python list
+        for col in text_columns:
+            alltext.extend(text_columns[col].dropna().astype(str).tolist())
 
-#Collect all text by a loop through each dataset
-for file in dataset:
-    #Read each CSV file
-    df = pd.read_csv(file)
+    #Convert all texts to a single .txt file
+    with open(output_file, 'w', encoding='utf-8') as result:
+        for text in alltext:
+            result.write(text + '\n')
     
-    #Extract the columns contain name "TEXT" in the column name
-    text_columns = df.filter(like = 'TEXT', axis = 1)
-    
-    #Convert the extracted columns from series to strings, then to python list
-    for col in text_columns:
-        alltext.extend(text_columns[col].dropna().astype(str).tolist())
+    #Print message after converting successfully 
+    print('All texts of 4 CSV files is successfully extracted to Q1_Task1_all_text_output.txt')
 
-#Convert all texts to a single .txt file
-with open('Q1_Task1_all_text_output.txt', 'w', encoding='utf-8') as result:
-    for text in alltext:
-        result.write(text + '\n')
-#Print message after converting successfully 
-print('All texts of 4 CSV files is successfully extracted to Q1_Task1_all_text_output.txt')
-    
-    
-    
+dataset = ['data/CSV1.csv', 'data/CSV2.csv', 'data/CSV3.csv', 'data/CSV4.csv']
+output_file = 'data/Q1_Task1_all_text_output.txt'
+extract_text_from_csv(dataset, output_file)    
     
--- Q2_Chapter1.py
+++ Q2_Chapter1.py
@@ -1,6 +1,5 @@
 #Question 2: Chapter 1 - The Gatekeeper
 #import libraries
-import pandas as pd
 import time
 from PIL import Image
 import numpy as np
@@ -14,22 +13,27 @@ if generated_number % 2 == 0:
     generated_number += 10
 print("Result of generated number is:",generated_number)
 
-
-#Change color of image with gerated_number
+#function to change color of image with gerated_number
 #Read image
-img = Image.open("data/chapter1.jpg","r")
-pixel = np.array(img)
+def change_color_image(input_image, output_image):
+    
+    img = Image.open(input_image, "r")
+    pixel = np.array(img)
 
+    #New pixels with generated_number
+    new_pixel = pixel + generated_number
+    
+    #esure new pixels values are with in [0, 255]
+    new_pixel = np.clip(new_pixel, 0, 255)
 
-#New pixels with generated_number
-new_pixel = pixel + generated_number
-#esure new pixels values are with in [0, 255]
-new_pixel = np.clip(new_pixel, 0, 255)
+    #Create a new image and save as chapter1output.png
+    new_img = Image.fromarray(new_pixel.astype("uint8"))
+    new_img.save(output_image)
 
-#Create a new image and save as chapter1output.png
-new_img = Image.fromarray(new_pixel.astype('uint8'))
-new_img.save("data/chapter1out.png")
+    # Calculate the sum of all red pixel values
+    red_pixel_sum = np.sum(new_pixel[:, :, 0])
+    print(f"Sum of all red pixel values: {red_pixel_sum}")
 
-# Calculate the sum of all red pixel values
-red_pixel_sum = np.sum(new_pixel[:, :, 0])
-print(f"Sum of all red pixel values: {red_pixel_sum}")
+input_image = "data/chapter1.jpg"
+output_image = "data/chapter1out.png"
+change_color_image(input_image, output_image)
